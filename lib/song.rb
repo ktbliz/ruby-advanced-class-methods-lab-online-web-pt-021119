c5f123a8d143283317b9acfd8c2c466dbb91c982
Katie Blizzard <kblizzard11@gmail.com>
2019-03-15 01:21:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,10 @@ class Song
   
   def self.find_or_create_by_name(name) 
     if self.find_by_name(name)
-      self.find_by_name()
+      self.find_by_name(name)
+    else 
+      self.create_by_name(name)
+    end 
   end 
 
 end
